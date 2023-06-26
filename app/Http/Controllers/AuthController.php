8b5030929e0615a61790074d20f2ec6c93d8a870
Oniikaji <55785264+Oniikaji@users.noreply.github.com>
2023-06-26 11:29:32 +0200

    Creating a register and now starting to make a login page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -10,4 +14,27 @@ class AuthController extends Controller
     {
         return view('auth/register');
     }
+
+    public function registerSave(Request $request)
+    {
+        Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ])->validate();
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('login');
+    }
+
+    public function login()
+    {
+        return view('auth/login');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,7 @@ Route::get('/', function () {
 
 Route::controller(AuthController::class)->group(function () {
     Route::get('register', 'register')->name('register');
+    Route::post('register', 'registerSave')->name('register.save');
+
+    Route::get('login', 'login')->name('login');
 });
